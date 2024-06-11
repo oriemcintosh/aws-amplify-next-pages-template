bbf9c9e4ea58b5c126af08c8fb81e2a4d19c3666
Orie McIntosh <dev@oriemcintosh.com>
2024-06-11 12:33:35 -0400

    Added option to delete tasks and user authentication

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
 
 const client = generateClient<Schema>();
 
@@ -11,6 +13,10 @@ export default function App() {
     client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
+  }
+ 
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
   }
 
   useEffect(() => {
@@ -24,21 +30,33 @@ export default function App() {
   }
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/gen2/start/quickstart/nextjs-pages-router/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
-    </main>
-  );
+        
+    <Authenticator>
+      {({ signOut, user }) => (
+        
+      <main>
+        <h1>My todos</h1>
+        <button onClick={createTodo}>+ new</button>
+        <ul>
+          {todos.map((todo) => (
+              
+            <li 
+            onClick={() => deleteTodo(todo.id)}
+            key={todo.id}>
+            {todo.content}
+            </li>
+          ))}
+        </ul>
+        <div>
+          🥳 App successfully hosted. Try creating a new todo.
+          <br />
+          <a href="https://docs.amplify.aws/gen2/start/quickstart/nextjs-pages-router/">
+            Review next steps of this tutorial.
+          </a>
+        </div>
+        <button onClick={signOut}>Sign out</button>
+      </main>
+    )}
+    </Authenticator>
+  )
 }
